42f10c4b76ac95cd4016ecc1b3e607c8640d6865
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2020-04-24 03:58:55 +0200

    Gestión de reservas.

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/negocio/mongodb/utilidades/MongoDB.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/negocio/mongodb/utilidades/MongoDB.java
@@ -1,12 +1,20 @@
 package org.iesalandalus.programacion.reservasaulas.mvc.modelo.negocio.mongodb.utilidades;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bson.Document;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Aula;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Permanencia;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.PermanenciaPorHora;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.PermanenciaPorTramo;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Profesor;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Reserva;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Tramo;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
@@ -17,11 +25,14 @@ import com.mongodb.client.MongoDatabase;
 
 public class MongoDB {
 	
+	public static final DateTimeFormatter FORMATO_DIA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	public static final DateTimeFormatter FORMATO_HORA = DateTimeFormatter.ofPattern("HH:mm");
+	
 	private static final String SERVIDOR = "35.198.132.204";
 	private static final int PUERTO = 27017;
 	private static final String BD = "reservasaulas";
-	private static final String USUARIO = "profesor";
-	private static final String CONTRASENA = "profesor-2020";
+	private static final String USUARIO = "reservasaulas";
+	private static final String CONTRASENA = "reservasaulas-2020";
 	
 	public static final String PROFESOR = "profesor";
 	public static final String NOMBRE = "nombre";
@@ -30,6 +41,20 @@ public class MongoDB {
 	
 	public static final String AULA = "aula";
 	public static final String PUESTOS = "puestos";
+	
+	public static final String PERMANENCIA = "permanencia";
+	public static final String TIPO = "tipo";
+	public static final String DIA = "dia";
+	public static final String HORA = "hora";
+	public static final String TRAMO = "tramo";
+	public static final String PROFESOR_CORREO = PROFESOR + "." + CORREO;
+	public static final String AULA_NOMBRE = AULA + "." + NOMBRE;
+	public static final String PERMANENCIA_TIPO = PERMANENCIA + "." + TIPO;
+	public static final String TIPO_HORA = "POR_HORA";
+	public static final String TIPO_TRAMO = "POR_TRAMO";
+	public static final String PERMANENCIA_DIA = PERMANENCIA + "." + DIA;
+	public static final String PERMANENCIA_TRAMO = PERMANENCIA + "." + TRAMO;
+	public static final String PERMANENCIA_HORA = PERMANENCIA + "." + HORA;
 	
 	private static MongoClient conexion = null;
 	
@@ -99,6 +124,47 @@ public class MongoDB {
 			return null;
 		}
 		return new Aula(documentoAula.getString(NOMBRE), documentoAula.getInteger(PUESTOS));
+	}
+	
+	public static Document obtenerDocumentoDesdeReserva(Reserva reserva) {
+		if (reserva == null) {
+			return null;
+		}
+		Profesor profesor = reserva.getProfesor();
+		Aula aula = reserva.getAula();
+		Permanencia permanencia = reserva.getPermanencia();
+		String dia = permanencia.getDia().format(FORMATO_DIA);
+		Document dProfesor = obtenerDocumentoDesdeProfesor(profesor);
+		Document dAula = obtenerDocumentoDesdeAula(aula);
+		Document dPermanencia = new Document().append(DIA, dia);
+		if (permanencia instanceof PermanenciaPorTramo) {
+			String tramo = ((PermanenciaPorTramo) permanencia).getTramo().name();
+			dPermanencia.append(TIPO, TIPO_TRAMO).append(TRAMO, tramo);
+		} else {
+			String hora = ((PermanenciaPorHora) permanencia).getHora().format(FORMATO_HORA);
+			dPermanencia.append(TIPO, TIPO_HORA).append(HORA, hora);
+		}
+		return new Document().append(PROFESOR, dProfesor).append(AULA, dAula).append(PERMANENCIA, dPermanencia);
+	}
+	
+	public static Reserva obtenerReservaDesdeDocumento(Document documentoReserva) {
+		if (documentoReserva == null) {
+			return null;
+		}
+		Aula aula = new Aula(obtenerAulaDesdeDocumento((Document) documentoReserva.get(AULA)));
+		Profesor profesor = new Profesor(obtenerProfesorDesdeDocumento((Document) documentoReserva.get(PROFESOR)));
+		Document dPermanencia = (Document) documentoReserva.get(PERMANENCIA);
+		LocalDate dia = LocalDate.parse(dPermanencia.getString(DIA), FORMATO_DIA);
+		String tipoPermanencia = dPermanencia.getString(TIPO);
+		Permanencia permanencia = null;
+		if (tipoPermanencia.equals(TIPO_TRAMO)) {
+			Tramo tramo = Tramo.valueOf(dPermanencia.getString(TRAMO));
+			permanencia = new PermanenciaPorTramo(dia, tramo);
+		} else {
+			LocalTime hora = LocalTime.parse(dPermanencia.getString(HORA), FORMATO_HORA);
+			permanencia = new PermanenciaPorHora(dia, hora);
+		}
+		return new Reserva(profesor, aula, permanencia);
 	}
 
 }
